ffba7d7d1f9ecceafa6c0a12111b3124df44b25f
unknown <adesanctis_be@dev7.devcodecamp.inc>
2015-11-04 15:42:19 -0600

    reply bot almost finished on Day1git push -u origin master

--- documents/DJJohnCenaBot/twitterBot.py
+++ documents/DJJohnCenaBot/twitterBot.py
@@ -1,5 +1,9 @@
+import _random
 import tweepy
+import tweepy,random,time
 from keys import keys 
+from datetime import datetime,timedelta
+from random import randint
 
 class TwitterAPI(object):
     def __init__(self):
@@ -11,9 +15,23 @@ class TwitterAPI(object):
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
 	self.twitterQuotesList = open('twitterquotes.txt', 'r')
+	self.tql = self.twitterQuotesList.readlines()
+	self.twitterQuotesList.close()
     def tweet(self):
-		for self.quote in self.twitterQuotesList:
-			self.api.update_status(status=self.quote)
+		while True:
+			trends1 = self.api.trends_place(2379574)
+			hashtags = [x['name'] for x in trends1[0]['trends'] if x['name'].startswith('#')]
+			trend_hashtag = hashtags[0]
+			tweetSearchResults = self.api.search(q=trend_hashtag,count=50)  
+			for tweet in tweetSearchResults:
+				sn = tweet.user.screen_name
+				message = self.tql[randint(0,len(self.tql)-1)] 
+				m = "@{0}, {1} {2}".format(sn, message,trend_hashtag) 
+				s = self.api.update_status(status=m, in_reply_to_status_id = tweet.id)
+				TimeToSleep = randint(3600,4000)
+				time.sleep(TimeToSleep)
+		#for self.quote in self.twitterQuotesList:
+			#self.api.update_status(status=self.quote)
 
 if __name__ == "__main__":
     twitter = TwitterAPI()
