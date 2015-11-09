029962e142a4b11c2db3a69f659ff82e5fc3b080
unknown <adesanctis_be@dev7.devcodecamp.inc>
2015-11-09 09:25:22 -0600

    final commit

--- documents/DJJohnCenaBot/keysOKitty.py
+++ documents/DJJohnCenaBot/keysOKitty.py
@@ -1,4 +1,4 @@
-keys1 = dict(
+keysOKitty = dict(
 	consumer_key = 'IYREsnkUy2wsBqx7db6iIwZGa',
 	consumer_secret = 'oXec7sMqr0HnnPUOIz2YO1007W0YaIk4kG4Uobc5e0I2GyVh8a',
 	access_token = '4128443473-Mdnu6z72Jn3vbcCkb3vpwUW99cwsVpnhDdO0s8s',
--- documents/DJJohnCenaBot/twitterBot.py
+++ documents/DJJohnCenaBot/twitterBot.py
@@ -1,38 +1,92 @@
 import _random
-import tweepy
+from tweepy import *
+from tweepy.auth import OAuthHandler
 import tweepy,random,time
-from keysSquall import keysSquall
+from tweepy import Stream
+from keysOKitty import keysOKitty
 from datetime import datetime,timedelta
 from random import randint
+import threading
+from threading import Thread
+from tweepy.streaming import StreamListener
+import sys
+import string
+import datetime
+now = datetime.datetime.now()
+day=int(now.day)
+month=int(now.month)
+year=int(now.year)
 
 class TwitterAPI(object):
-    def __init__(self):
-        consumer_key = keysSquall['consumer_key']
-        consumer_secret = keysSquall['consumer_secret']
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        access_token = keysSquall['access_token']
-        access_token_secret = keysSquall['access_token_secret']
-        auth.set_access_token(access_token, access_token_secret)
-        self.api = tweepy.API(auth)
-	self.twitterQuotesList = open('squallquotes.txt', 'r')
-	self.tql = self.twitterQuotesList.readlines()
-	self.twitterQuotesList.close()
-    def tweet(self):
-		while True:
-			trends1 = self.api.trends_place(2379574)
-			hashtags = [x['name'] for x in trends1[0]['trends'] if x['name'].startswith('#')]
-			trend_hashtag = hashtags[0]
-			tweetSearchResults = self.api.search(q=trend_hashtag,count=50)  
-			for tweet in tweetSearchResults:
-				sn = tweet.user.screen_name
-				message = self.tql[randint(0,len(self.tql)-1)] 
-				m = "@{0}, {1} {2}".format(sn, message,trend_hashtag) 
-				s = self.api.update_status(status=m, in_reply_to_status_id = tweet.id)
-				TimeToSleep = randint(1500,3000)
-				time.sleep(TimeToSleep)
-		#for self.quote in self.twitterQuotesList:
-			#self.api.update_status(status=self.quote)
+   def __init__(self):
+      consumer_key = keysOKitty['consumer_key']
+      consumer_secret = keysOKitty['consumer_secret']
+      auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+      access_token = keysOKitty['access_token']
+      access_token_secret = keysOKitty['access_token_secret']
+      auth.set_access_token(access_token, access_token_secret)
+      self.api = tweepy.API(auth)
+      self.twitterQuotesList = open('kittyquotes.txt', 'r')
+      self.tql = self.twitterQuotesList.readlines()
+      self.twitterQuotesList.close()
+      self.tweetQuery = ""
+      self.tweetSearchResults = ""
+      self.trends1 = ""
+      self.hashtagsForTrends = ""
+      self.hashtagsForKeywords = ""
+      self.trend_hashtag = ""
+      self.keyword_hashtag = ""
+      self.keywords = ""
+      self.specificKeywords = ""
+      
+   def searchTrends(self):
+       self.trends1 = self.api.trends_place(2436704)
+       self.hashtagsForTrends = [x['name'] for x in self.trends1[0]['trends'] if x['name'].startswith('#')]
+       self.trend_hashtag = self.hashtagsForTrends[0]
+       self.tweetSearchResults = self.api.search(q=self.trend_hashtag,count=100)
+       
+   # def searchKeyword(self):
+       # self.keywords = self.api.["Video", "Game", "Games"]
+       # self.hashtagsForKeywords = [x['name'] for x in self.keywords[0,len(self.keywords)]['trends'] if x['name'].startswith('#')]
+       # self.keyword_hashtag = self.hashtagsForKeywords[0]
+       # self.tweetQuery = self.api.search(q=self.keywords, count=5)
+   
+   def trendingTweet(self):
+       for tweet in self.tweetSearchResults:
+           sn = tweet.user.screen_name
+           message = self.tql[randint(0,len(self.tql)-1)] 
+           m = "@{0}, {1} {2}".format(sn, message,self.trend_hashtag) 
+           s = self.api.update_status(status=m, in_reply_to_status_id = tweet.id)
+           TimeToSleep = randint(1500,3000)
+           time.sleep(TimeToSleep)
+
+   # def keywordTweet(self):
+       # for tweet1 in self.tweetQuery:
+           # sn = tweet.user.screen_name
+           # message = self.tql[randint(0,len(self.tql)-1)] 
+           # m = "@{0}, {1} {2}".format(sn, message,self.trend_hashtag) 
+           # s = self.api.update_status(status=m, in_reply_to_status_id = tweet.id)
+           # TimeToSleep = randint(1500,3000)
+           # time.sleep(TimeToSleep)
+           
+   def collectData(self):
+       statuses = tweepy.Cursor(self.api.home_timeline).items(5)
+       for status in statuses:
+           outfp = open("TwitterData.txt", "w")
+           outfp.write(status.text)
+           outfp.close()
+               
+           
+      
+       
+       
+       #self.query = self.api.search.tweets(q = "Crazy lady")
+       #for self.quote in self.twitterQuotesList:
+       #self.api.update_status(status=self.quote)
 
 if __name__ == "__main__":
-    twitter = TwitterAPI()
-    twitter.tweet()
+  twitter = TwitterAPI()
+  while True:
+      twitter.collectData()
+      twitter.searchTrends()
+      twitter.trendingTweet()
